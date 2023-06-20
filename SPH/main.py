f08ccad266a890436265a9312badeeef365b27c4
chunleili <li_cl@foxmail.com>
2023-06-20 14:07:21 +0800

    [refac] extract make_domainbox

--- SPH/main.py
+++ SPH/main.py
@@ -1058,6 +1058,23 @@ class DFSPHSolver(SPHBase):
         self.advect()
 
 
+def make_doaminbox():
+    # Draw the lines for domain
+    x_max, y_max, z_max = get_cfg("domainEnd")
+    box_anchors = ti.Vector.field(3, dtype=ti.f32, shape=8)
+    box_anchors[0] = ti.Vector([0.0, 0.0, 0.0])
+    box_anchors[1] = ti.Vector([0.0, y_max, 0.0])
+    box_anchors[2] = ti.Vector([x_max, 0.0, 0.0])
+    box_anchors[3] = ti.Vector([x_max, y_max, 0.0])
+    box_anchors[4] = ti.Vector([0.0, 0.0, z_max])
+    box_anchors[5] = ti.Vector([0.0, y_max, z_max])
+    box_anchors[6] = ti.Vector([x_max, 0.0, z_max])
+    box_anchors[7] = ti.Vector([x_max, y_max, z_max])
+    box_lines_indices = ti.field(int, shape=(2 * 12))
+    for i, val in enumerate([0, 1, 0, 2, 1, 3, 2, 3, 4, 5, 4, 6, 5, 7, 6, 7, 0, 4, 1, 5, 2, 6, 3, 7]):
+        box_lines_indices[i] = val
+    return box_anchors, box_lines_indices
+
 # ---------------------------------------------------------------------------- #
 #                                     main                                     #
 # ---------------------------------------------------------------------------- #
@@ -1085,24 +1102,7 @@ def main():
     background_color = (0, 0, 0)  # 0xFFFFFF
     particle_color = (1, 1, 1)
 
-    # Draw the lines for domain
-    x_max, y_max, z_max = get_cfg("domainEnd")
-    box_anchors = ti.Vector.field(3, dtype=ti.f32, shape=8)
-    box_anchors[0] = ti.Vector([0.0, 0.0, 0.0])
-    box_anchors[1] = ti.Vector([0.0, y_max, 0.0])
-    box_anchors[2] = ti.Vector([x_max, 0.0, 0.0])
-    box_anchors[3] = ti.Vector([x_max, y_max, 0.0])
-    box_anchors[4] = ti.Vector([0.0, 0.0, z_max])
-    box_anchors[5] = ti.Vector([0.0, y_max, z_max])
-    box_anchors[6] = ti.Vector([x_max, 0.0, z_max])
-    box_anchors[7] = ti.Vector([x_max, y_max, z_max])
-    box_lines_indices = ti.field(int, shape=(2 * 12))
-    for i, val in enumerate([0, 1, 0, 2, 1, 3, 2, 3, 4, 5, 4, 6, 5, 7, 6, 7, 0, 4, 1, 5, 2, 6, 3, 7]):
-        box_lines_indices[i] = val
-
-    # rb_pos = read_ply_particles(sph_root_path + "/data/models/cube.ply")
-    # rb = RigidBody(rb_pos)
-    # rb.rotation(60, rb.positions)
+    box_anchors, box_lines_indices = make_doaminbox()
 
     cnt = 0
     meta.paused = False
@@ -1113,7 +1113,6 @@ def main():
                 print("paused:", meta.paused)
         if not meta.paused:
             solver.step()
-            # rb.substep()
 
         camera.track_user_inputs(window, movement_speed=movement_speed, hold_key=ti.ui.RMB)
         scene.set_camera(camera)
